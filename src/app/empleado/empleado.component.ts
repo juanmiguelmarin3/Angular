e5812dc2d00fdeab65d6c783c10c867e82ee71fa
Juan Miguel <marincortezj@gmail.com>
2023-01-20 11:34:14 -0500

    Uploaded

---
+++ src/app/empleado/empleado.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empleado',
+  templateUrl: './empleado.component.html',
+  styleUrls: ['./empleado.component.css']
+  //styles: ["p{background-color:red;}"] //Estilo inline
+})
+export class EmpleadoComponent {
+
+}
